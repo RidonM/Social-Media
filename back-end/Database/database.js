a683ae06d53e3b9c10dfd6bf77e2e0bfa993d7a3
RidonM <ridonmaliqi5@gmail.com>
2024-12-18 18:34:55 +0100

    add not null to users

--- back-end/Database/database.js
+++ back-end/Database/database.js
@@ -16,18 +16,23 @@ const User = sequelize.define(
     },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     surname: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     username: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     password: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
   },
   {
